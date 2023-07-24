305edc3d7c02966d15b5d42ed34e0704bfd7e02b
Nathan Baldez <nathanbsb2@hotmail.com>
2023-07-23 21:40:22 -0300

    refactor: page wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,8 @@
 
 import { ProductsList } from '@/components/ProductsList';
 import FilterBar from '@/components/FilterBar';
-import styled from 'styled-components';
 import { DefaultPageLayout } from '@/components/DefaultPageLayout';
-
-export const PageWrapper = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
+import { PageWrapper } from '@/components/PageWrapper';
 
 export default function Home() {
   return (
